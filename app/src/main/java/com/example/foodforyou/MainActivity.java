de347ac77dde9100498e716a6af514a8ae2aa70a
fathan_ <arrazy.rabbani266@gmail.com>
2019-11-29 11:09:21 +0700

    Add Navigation Bar

--- app/src/main/java/com/example/foodforyou/MainActivity.java
+++ app/src/main/java/com/example/foodforyou/MainActivity.java
@@ -1,14 +1,62 @@
 package com.example.foodforyou;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.FrameMetrics;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Load Fragment
+        loadFragment(new HomeFragment());
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.main_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
     }
+
+    private boolean loadFragment(Fragment fragment){
+        if (fragment != null){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.main_frame, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.nav_rekomendasi:
+                fragment = new RekomendasiFragment();
+                break;
+            case R.id.nav_tips:
+                fragment = new TipsFragment();
+                break;
+            case R.id.nav_profil:
+                fragment = new ProfileFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+    public void setActionBarTitle(String title) {
+        getSupportActionBar().setTitle(title);
+    }
+
 }
+
