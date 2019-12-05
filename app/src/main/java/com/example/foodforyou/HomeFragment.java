a5711f61beed7b19294a39d8215f139b7a41a211
arrazyfathan <arrazy.rabbani266@gmail.com>
2019-12-05 15:16:08 +0700

    View Data BMR and BMI

--- app/src/main/java/com/example/foodforyou/HomeFragment.java
+++ app/src/main/java/com/example/foodforyou/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.foodforyou;
 
 
+import android.database.Cursor;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -21,11 +23,14 @@ public class HomeFragment extends Fragment {
     }
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+
+
     }
 
 
@@ -35,5 +40,36 @@ public class HomeFragment extends Fragment {
         super.onResume();
 
         ((MainActivity) getActivity()).setActionBarTitle("Home");
+        getGoal();
+    }
+
+
+    public void getGoal(){
+        /* Database */
+        DBAdapter db = new DBAdapter(getActivity());
+        db.open();
+
+        String fieldsGoal[] = new String[]{
+                "_id",
+                "goal_energy_with_activity_and_diet",
+                "goal_bmi"
+        };
+        Cursor cursorGoal = db.select("goal", fieldsGoal);
+        cursorGoal.moveToLast();
+        String stringGoalEnergyWithActivityAndDiet = cursorGoal.getString(1);
+        String stringBmi = cursorGoal.getString(2);
+
+        // TextView goal
+        TextView textViewBodyGoalWithActivity = (TextView) getActivity().findViewById(R.id.jumlah_kalori);
+        textViewBodyGoalWithActivity.setText(stringGoalEnergyWithActivityAndDiet);
+
+        //TextView bmi
+        TextView textViewBmi = (TextView) getActivity().findViewById(R.id.bmi);
+        textViewBmi.setText(stringBmi);
+
+
+        db.close();
+
+
     }
 }
