18415d9c3eac81102308200835ac023ddfc83487
dionofrizal88 <dioagusn88@gmail.com>
2019-12-05 16:47:52 +0700

    toolbar in fragment

--- app/src/main/java/com/example/foodforyou/ProfileFragment.java
+++ app/src/main/java/com/example/foodforyou/ProfileFragment.java
@@ -6,8 +6,14 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import javax.annotation.Nullable;
 
 
 /**
@@ -32,5 +38,25 @@ public class ProfileFragment extends Fragment {
         super.onResume();
 
         ((MainActivity) getActivity()).setActionBarTitle("Profile");
+    }
+
+//    membuat toolbar
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState){
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
+@Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        inflater.inflate(R.menu.setting_profile, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+@Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.action_logout){
+            Toast.makeText(getActivity(), "Logout", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
